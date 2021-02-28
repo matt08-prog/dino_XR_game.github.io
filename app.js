61081a5b9c9597662f71bcda28768befe0e14915
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-02-28 14:04:30 -0700

    push

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ class App{
                         child.material.metalness = 0.2;
                     }
                 })
+                
                 // self.chair = gltf.scene;
                 
 				self.scene.add( gltf.scene );
@@ -181,36 +182,36 @@ class App{
             
             case 'tracked-pointer':
 
-                loader = new GLTFLoader().setPath('./Assets/');
+                // loader = new GLTFLoader().setPath('./Assets/');
         
-                loader.load( 'flash-light.glb',
-                    ( gltf ) => {
-                        const flashLight = gltf.scene.children[2];
-                        const scale = 0.6;
-                        flashLight.scale.set(scale, scale, scale);
-                        controller.add( flashLight );
-                        self.spotlight = new THREE.Group();
-                        const spotlight = new THREE.SpotLight( 0xFFFFFF, 2, 12, Math.PI/15, 0.3 );
-                        geometry = new THREE.CylinderBufferGeometry(0.03, 1, 5, 32, 5, true);
-                        geometry.rotateX( Math.PI/2 );
-                        material = new SpotLightVolumetricMaterial();
-                        const cone = new THREE.Mesh( geometry, material );
-                        cone.translateZ( -2.6 );
+                // loader.load( 'flash-light.glb',
+                //     ( gltf ) => {
+                //         const flashLight = gltf.scene.children[2];
+                //         const scale = 0.6;
+                //         flashLight.scale.set(scale, scale, scale);
+                //         controller.add( flashLight );
+                //         self.spotlight = new THREE.Group();
+                //         const spotlight = new THREE.SpotLight( 0xFFFFFF, 2, 12, Math.PI/15, 0.3 );
+                //         geometry = new THREE.CylinderBufferGeometry(0.03, 1, 5, 32, 5, true);
+                //         geometry.rotateX( Math.PI/2 );
+                //         material = new SpotLightVolumetricMaterial();
+                //         const cone = new THREE.Mesh( geometry, material );
+                //         cone.translateZ( -2.6 );
                     
-                        spotlight.position.set(0,0,0);
-                        spotlight.target.position.set(0,0,-1);
-                        self.spotlight.add( spotlight.target );
-                        self.spotlight.add( spotlight );
-                        self.spotlight.add( cone );
+                //         spotlight.position.set(0,0,0);
+                //         spotlight.target.position.set(0,0,-1);
+                //         self.spotlight.add( spotlight.target );
+                //         self.spotlight.add( spotlight );
+                //         self.spotlight.add( cone );
                         
-                        controller.add(self.spotlight);
-                        self.spotlight.visible = false;
-                    },
-                    null,
-                    (error) =>  {
-                        console.error( 'An error occurred' );    
-                    }
-                );
+                //         controller.add(self.spotlight);
+                //         self.spotlight.visible = false;
+                //     },
+                //     null,
+                //     (error) =>  {
+                //         console.error( 'An error occurred' );    
+                //     }
+                // );
                 
                 break;
                 
