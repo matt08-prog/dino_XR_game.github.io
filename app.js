40d999581acb9a7f185f849863a03bec7b786100
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-02-28 20:43:30 -0700

    controller input

--- app.js
+++ app.js
@@ -67,32 +67,32 @@ class App{
     initScene(){
         this.dir = 0
 
-        this.radius = 0.08;
+        // this.radius = 0.08;
         
-        this.room = new THREE.LineSegments(
-					new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ),
-					new THREE.LineBasicMaterial( { color: 0x808080 } )
-				);
-        this.room.geometry.translate( 0, 3, 0 );
-        this.scene.add( this.room );
+        // this.room = new THREE.LineSegments(
+		// 			new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ),
+		// 			new THREE.LineBasicMaterial( { color: 0x808080 } )
+		// 		);
+        // this.room.geometry.translate( 0, 3, 0 );
+        // this.scene.add( this.room );
         
-        const geometry = new THREE.IcosahedronBufferGeometry( this.radius, 2 );
+        // const geometry = new THREE.IcosahedronBufferGeometry( this.radius, 2 );
 
-        for ( let i = 0; i < 200; i ++ ) {
+        // for ( let i = 0; i < 200; i ++ ) {
 
-            const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+        //     const object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
 
-            object.position.x = this.random( -2, 2 );
-            object.position.y = this.random( -2, 2 );
-            object.position.z = this.random( -2, 2 );
+        //     object.position.x = this.random( -2, 2 );
+        //     object.position.y = this.random( -2, 2 );
+        //     object.position.z = this.random( -2, 2 );
 
-            this.room.add( object );
+        //     this.room.add( object );
 
-        }
+        // }
 
-        this.highlight = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.BackSide } ) );
-        this.highlight.scale.set(1.2, 1.2, 1.2);
-        this.scene.add(this.highlight);
+        // this.highlight = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.BackSide } ) );
+        // this.highlight.scale.set(1.2, 1.2, 1.2);
+        // this.scene.add(this.highlight);
 
         this.loadGLTF()
         this.ui = this.createUI();
@@ -397,14 +397,14 @@ class App{
 
             const intersects = this.raycaster.intersectObjects( this.room.children );
 
-            if (intersects.length>0){
-                intersects[0].object.add(this.highlight);
-                this.highlight.visible = true;
-                controller.children[0].scale.z = intersects[0].distance;
-                controller.userData.selected = intersects[0].object;
-            }else{
-                this.highlight.visible = false;
-            }
+            // if (intersects.length>0){
+            //     intersects[0].object.add(this.highlight);
+            //     this.highlight.visible = true;
+            //     controller.children[0].scale.z = intersects[0].distance;
+            //     controller.userData.selected = intersects[0].object;
+            // }else{
+            //     this.highlight.visible = false;
+            // }
         }
     }
     
