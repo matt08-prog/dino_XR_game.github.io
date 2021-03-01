1faffcc1e4f9590afbe8a2ac2de747fe088a6e87
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-02-28 21:08:46 -0700

    controller input

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ class App{
                 const scale = 0.07;
                 self.earth.scale.set(scale, scale, scale);
                 self.earth.position.y = 1.2
-                self.earth.position.z = -1.0
+                self.earth.position.z = -1.2
                 gltf.scene.traverse( ( child ) => {
                     if (child.isMesh){
                         child.material.metalness = 0.2;
