fd1321e8832bc8b93941fe15cf047ce18468b54d
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-03-02 18:21:52 -0700

    added node function

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ class App{
     loadGLTF(){
         const loader = new GLTFLoader( ).setPath('./Assets/');
         const self = this;
-        this.addNode(0.4)
+        this.addNode(0.36)
 
 		// Load a glTF resource
         self.objects = []
