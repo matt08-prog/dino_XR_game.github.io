67ac88c998f896ca088febb215009474e458d1ff
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-03-12 23:53:37 -0700

    final pos is correct node

--- app.js
+++ app.js
@@ -226,13 +226,13 @@ class App{
             const info = {};
             console.log("playing out")
 
-            // self.listener = new THREE.AudioListener()
-            // self.sound = new THREE.Audio( self.listener )
-            // self.mediaElement = new Audio("https://cors-anywhere.herokuapp.com/http://radio.garden/api/ara/content/listen/lWw8pNel/channel.mp3")
-            // self.mediaElement.crossOrigin = "anonymous"
-            // self.mediaElement.loop = true
-            // mediaElement.play()
-            // self.sound.setMediaElementSource( self.mediaElement )
+            self.listener = new THREE.AudioListener()
+            self.sound = new THREE.Audio( self.listener )
+            self.mediaElement = new Audio("https://cors-anywhere.herokuapp.com/http://radio.garden/api/ara/content/listen/lWw8pNel/channel.mp3")
+            self.mediaElement.crossOrigin = "anonymous"
+            self.mediaElement.loop = true
+            mediaElement.play()
+            self.sound.setMediaElementSource( self.mediaElement )
 
             // self.setSound("https://cors-anywhere.herokuapp.com/http://radio.garden/api/ara/content/listen/lWw8pNel/channel.mp3")
             // self.sound.setRefDistance(20)
@@ -249,14 +249,14 @@ class App{
             //     console.log("playing")
             // })
 
-            self.listener = new THREE.AudioListener()
-            self.sound = new THREE.Audio( self.listener )
-            const file = "https://cors-anywhere.herokuapp.com/http://radio.garden/api/ara/content/listen/lWw8pNel/channel.mp3"
-            self.mediaElement = new Audio( file );
-            self.mediaElement.crossOrigin = "anonymous"
-            self.mediaElement.loop = true
-            self.mediaElement.play();
-            self.sound.setMediaElementSource( self.mediaElement );
+            // self.listener = new THREE.AudioListener()
+            // self.sound = new THREE.Audio( self.listener )
+            // const file = "https://cors-anywhere.herokuapp.com/http://radio.garden/api/ara/content/listen/lWw8pNel/channel.mp3"
+            // self.mediaElement = new Audio( file );
+            // self.mediaElement.crossOrigin = "anonymous"
+            // self.mediaElement.loop = true
+            // self.mediaElement.play();
+            // self.sound.setMediaElementSource( self.mediaElement );
 
             fetchProfile( event.data, DEFAULT_PROFILES_PATH, DEFAULT_PROFILE ).then( ( { profile, assetPath } ) => {                
                 info.name = profile.profileId;
