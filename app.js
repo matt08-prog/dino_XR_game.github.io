9d71c5129a5ea9f7be03775b8430fb04680d1358
matt08-prog <58103083+matt08-prog@users.noreply.github.com>
2021-03-15 22:11:08 -0600

    final pos is correct node

--- app.js
+++ app.js
@@ -241,23 +241,21 @@ class App{
             //     Feed the HTMLMediaElement into it
                 self.source = audioCtx.createMediaElementSource(self.myAudio);
 
-            //      Create a stereo panner
-                self.panNode = audioCtx.createStereoPanner();
+            // //      Create a stereo panner
+            //     self.panNode = audioCtx.createStereoPanner();
                 
-                self.panNode.pan.value = 0;
+            //     self.panNode.pan.value = 0;
                 
-            //      connect the AudioBufferSourceNode to the gainNode
-            //      and the gainNode to the destination, so we can play the
-            //      music and adjust the panning using the controls
-                self.source.connect(self.panNode);
-                self.panNode.connect(audioCtx.destination);
+            // //      connect the AudioBufferSourceNode to the gainNode
+            // //      and the gainNode to the destination, so we can play the
+            // //      music and adjust the panning using the controls
+            //     self.source.connect(self.panNode);
+            //     self.panNode.connect(audioCtx.destination);
 
-                 self.myAudio.src = "https://cors-anywhere.herokuapp.com/http://radio.garden/api/ara/content/listen/lWw8pNel/channel.mp3"
-
-                 self.listener = new THREE.AudioListener()
-                 self.sound = new THREE.Audio( self.listener )
-                 self.myAudio.play()
-                 self.sound.setMediaElementSource( self.myAudio )
+            //      self.listener = new THREE.AudioListener()
+            //      self.sound = new THREE.Audio( self.listener )
+            //      self.myAudio.play()
+            //      self.sound.setMediaElementSource( self.myAudio )
             }
 
 
